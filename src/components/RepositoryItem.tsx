98420c4285075c8ec93c6ec7a872472f158b1813
crisfeitosa <cris.immobilis@gmail.com>
2021-05-06 16:16:42 -0300

    Create github explorer

---
+++ src/components/RepositoryItem.tsx
@@ -0,0 +1,20 @@
+interface RepositoryItemProps {
+  repository: {
+    name: string;
+    description: string;
+    html_link: string;
+  }
+}
+
+export function RepositoryItem(props: RepositoryItemProps) {
+  return (
+    <li>
+      <strong>{props.repository.name}</strong>
+      <p>{props.repository.description}</p>
+    
+      <a href={props.repository.html_link}>
+        Acessar repositório
+      </a>
+    </li>
+  )
+}
